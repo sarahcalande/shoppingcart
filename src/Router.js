ecf9623ba42db35e6253648dff9204b4838728b7
sarahcalande <scalande@yahoo.com>
2019-01-05 16:49:47 -0500

    order page done

--- src/Router.js
+++ src/Router.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom'
 import HomePage from './HomePage.js'
 import Cart from './Cart.js'
 import CheckoutPage from './checkoutpage.js'
+import OrdersPage from './orderspage.js'
 
 const Router =()=>(
 <Switch>
 <Route exact path='/' component = {HomePage} />
 <Route exact path='/cart' component = {Cart} />
 <Route exact path='/checkout' component = {CheckoutPage} />
+<Route path='/orders/:id' component = {OrdersPage} />
 </Switch>
 )
 
---
+++ src/orderspage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Order from './features/order'
+
+
+
+export default function OrdersPage(props){
+  return<div>
+  <h2>My Order</h2>
+  <Order id={props.match.params.id}/></div>
+}
